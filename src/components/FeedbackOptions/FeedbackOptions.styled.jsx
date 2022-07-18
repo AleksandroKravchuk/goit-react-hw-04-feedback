50ba58e5540f4c41c875a04ae621ae0d2d3d1a15
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-18 10:04:44 +0300

    1

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+export const Button = styled.button`
+  font-size: 25px;
+  text-transform: capitalize;
+  text-align: center;
+  color: palevioletred;
+  padding: 15px;
+  border-radius: 10px
+  margin-right:30px;
+ cursor: pointer;
+ border:none;
+ background-color: #cdc9c9;
+  transition-property: transform, box-shadow;
+  transition-duration: 0.25s;
+  transition-timing-function: 0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98);
+ &:hover {
+    box-shadow: 0 9px 47px 11px rgba(51, 51, 51, 0.18);}
+`
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StatisticsSection = styled.div`
+    text-align:start;
+`
+
+export const StatisticItem = styled.p`
+    color: goldenrod;
+    font-size:25px;
+`
+export const Container = styled.div`
+    text-align:center;
+`
